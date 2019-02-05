3b62b9d97151e9a8de784ed9f837f01aa5b71a83
Alexey-Ivantsov <Squirrell97@gmail.com>
2019-02-05 16:36:28 +0200

    --

---
+++ Asp_Net_Chat/Asp_Net_Chat/AspChat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Asp_Net_Chat
+{
+    public class AspChat : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+    }
+}
