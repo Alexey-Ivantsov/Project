521e676195fade1d88086aed3e904652c04e1e44
Alexey-Ivantsov <Squirrell97@gmail.com>
2019-02-26 18:30:34 +0200

    --

--- Wpf_BattleShip/Wpf_BattleShip/MainWindow.xaml.cs
+++ Wpf_BattleShip/Wpf_BattleShip/MainWindow.xaml.cs
@@ -26,8 +26,6 @@ namespace Wpf_BattleShip
         int ComputerCount;
         public Grid[,] fieldsEnemy;
         public Grid[,] fieldsPlayer;
-        TypeShip typeShip = 0;
-        Orientations orientations;
         public int fourDeckCount = Const.FourDeck;
         public int threeDeckCount = Const.ThreeDeck;
         public int doubleDeckCount = Const.DoubleDeck;
@@ -35,30 +33,147 @@ namespace Wpf_BattleShip
         public int fourDeckHit = 0;
         public int threeDeckHit = 0;
         public int doubleDeckHit = 0;
-        bool singleDeckbool;
-        bool doubleDeckbool;
-        bool threeDeckbool;
-        bool fourDeckbool;
+
+        Game game;
         public MainWindow()
         {
             InitializeComponent();
             fieldsEnemy = FieldEnemy.field;
             fieldsPlayer = FieldPlayer.field;
-
+            ComputerCount = Const.COMPUTER_START;
             Print.PrintGrid(FieldPlayer.field, FieldEnemy.field);
-            Game game = new Game(FieldPlayer.field, FieldEnemy.field);
+            game = new Game(FieldPlayer.field, FieldEnemy.field);
         }
         private void sk(object sender, RoutedEventArgs e)
         {
             var element = (UIElement)e.Source;
             int c = Grid.GetColumn(element);
-            if (c == 0)
+            if (c == 1)
             {
-
+                foreach (var item in fieldsEnemy)
+                {
+                    item.MouseDown += ShootEnemyGrid;
+                }
             }
             // MessageBox.Show(c.ToString());
 
 
+        }
+        public void ShootEnemyGrid(object sender, MouseButtonEventArgs e)
+        {
+            Grid square = (Grid)sender;
+            int j = Grid.GetColumn(square) - 1;
+            int i = Grid.GetRow(square) - 1;
+            if (fieldsEnemy[i, j].Tag.Equals(Status.OccupiedComputer))
+            {
+                ComputerCount--;
+                fieldsEnemy[i, j].Tag = Status.Hit;
+                FindHit(fieldsEnemy);
+                MessageBox.Show("Вы потопили однопалубный корабль!");
+                Print.PrintGrid(fieldsPlayer, fieldsEnemy);
+                IsWin();
+                return;
+            }
+            else if (fieldsEnemy[i, j].Tag.Equals(Status.OccupiedComputer2))
+            {
+                ComputerCount--;
+                fieldsEnemy[i, j].Tag = Status.Hit;
+                doubleDeckHit++;
+                if (doubleDeckHit == 2)
+                {
+                    FindHit(fieldsEnemy);
+                    MessageBox.Show("Вы потопили двухпалубный корабль!");
+                    doubleDeckHit = 0;
+                }
+                Print.PrintGrid(fieldsPlayer, fieldsEnemy);
+                IsWin();
+                return;
+            }
+            else if (fieldsEnemy[i, j].Tag.Equals(Status.OccupiedComputer3))
+            {
+                ComputerCount--;
+                fieldsEnemy[i, j].Tag = Status.Hit;
+                threeDeckHit++;
+                if (threeDeckHit == 3)
+                {
+                    FindHit(fieldsEnemy);
+                    MessageBox.Show("Вы потопили трёхпалубный корабль!");
+                    threeDeckHit = 0;
+                }
+                Print.PrintGrid(fieldsPlayer, fieldsEnemy);
+                IsWin();
+                return;
+            }
+            else if (fieldsEnemy[i, j].Tag.Equals(Status.OccupiedComputer4))
+            {
+                ComputerCount--;
+                fieldsEnemy[i, j].Tag = Status.Hit;
+                fourDeckHit++;
+                if (fourDeckHit == 4)
+                {
+                    FindHit(fieldsEnemy);
+                    MessageBox.Show("Вы потопили четырёхпалубный корабль!");
+                    fourDeckHit = 0;
+                }
+                Print.PrintGrid(fieldsPlayer, fieldsEnemy);
+                IsWin();
+                return;
+            }
+            else if (fieldsEnemy[i, j].Tag.Equals(Status.Empty))
+            {
+                fieldsEnemy[i, j].Tag = Status.Used;
+                game.computer.Hit(fieldsPlayer);
+                Print.PrintGrid(fieldsPlayer, fieldsEnemy);
+                IsWin();
+                return;
+            }
+        }
+
+        public void FindHit(Grid[,] fieldsEnemy)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (fieldsEnemy[i, j].Tag.Equals(Status.Hit))
+                    {
+                        for (int k = -1; k < 2;)
+                        {
+                            for (int l = -1; l < 2;)
+                            {
+                                if (i + k < Const.MIN_BOUND || i + k > Const.MAX_BOUND || j + l < Const.MIN_BOUND || j + l > Const.MAX_BOUND)
+                                {
+                                    l++;
+                                }
+                                else
+                                {
+                                    if (!fieldsEnemy[i + k, j + l].Tag.Equals(Status.Hit) && !fieldsEnemy[i + k, j + l].Tag.Equals(Status.Occupied) && !fieldsEnemy[i + k, j + l].Tag.Equals(Status.Occupied2) && !fieldsEnemy[i + k, j + l].Tag.Equals(Status.Occupied3) && !fieldsEnemy[i + k, j + l].Tag.Equals(Status.Occupied4))
+                                    {
+                                        fieldsEnemy[i + k, j + l].Tag = Status.Used;
+
+                                    }
+                                    l++;
+                                }
+
+                            }
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        public void IsWin()
+        {
+            if (game.computer._hitInformation.PlayerCount == 0)
+            {
+                MessageBox.Show("Компьютер выиграл!");
+                // Stack.IsEnabled = false;
+            }
+            if (ComputerCount == 0)
+            {
+                MessageBox.Show("Вы выиграли!");
+                // Stack.IsEnabled = false;
+            }
         }
         private void button_Click(object sender, RoutedEventArgs e)
         {
